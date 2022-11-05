f809be67150ca57e475c8918c5b706b117ee14f3
aksun1 <aksunpostit@gmail.com>
2022-11-05 12:32:18 +0200

    Add local database support

---
+++ README.md
@@ -0,0 +1,6 @@
+# Junction app
+
+## Tools
+### Run migration
+
+    docker-compose exec backend node models/operator.js
--- packages/backend/app.js
+++ packages/backend/app.js
@@ -1,9 +1,32 @@
 const express = require('express')
+require("dotenv").config();
+const mysql = require('mysql2')
+const Sequelize = require("sequelize");
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+   
+
+    const sequelize = new Sequelize(
+      process.env.MYSQL_DATABASE,
+      'root',
+      process.env.MYSQL_ROOT_PASSWORD,
+        {
+          host: 'db',
+          dialect: 'mysql'
+        }
+      );
+
+    sequelize.authenticate().then(() => {
+      console.log('Connection has been established successfully.');
+    }).catch((error) => {
+      console.error('Unable to connect to the database: ', error);
+    });
+
+  
+  res.end("asd")
 })
 
 app.listen(port, () => {
