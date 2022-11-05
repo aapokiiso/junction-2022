6308a74fd9140280e6011ec087019ca2e5a50ee9
aksun1 <aksunpostit@gmail.com>
2022-11-05 18:31:16 +0200

    Add test data

---
+++ db/links.sql
@@ -0,0 +1,39 @@
+CREATE TABLE links(
+   id        INTEGER PRIMARY KEY AUTO_INCREMENT,
+   source VARCHAR(200) NOT NULL
+  ,target VARCHAR(200) NOT NULL
+);
+INSERT INTO links(source,target) VALUES ('St1','1');
+INSERT INTO links(source,target) VALUES ('1','2');
+INSERT INTO links(source,target) VALUES ('1','5');
+INSERT INTO links(source,target) VALUES ('2','Konetalo');
+INSERT INTO links(source,target) VALUES ('2','3');
+INSERT INTO links(source,target) VALUES ('3','Kandikeskus');
+INSERT INTO links(source,target) VALUES ('3','4');
+INSERT INTO links(source,target) VALUES ('4','Nanotalo');
+INSERT INTO links(source,target) VALUES ('5','6');
+INSERT INTO links(source,target) VALUES ('6','Kvarkki');
+INSERT INTO links(source,target) VALUES ('6','7');
+INSERT INTO links(source,target) VALUES ('7','Sähkötalo');
+INSERT INTO links(source,target) VALUES ('7','8');
+INSERT INTO links(source,target) VALUES ('8','Raksatalo');
+INSERT INTO links(source,target) VALUES ('8','9');
+INSERT INTO links(source,target) VALUES ('9','10');
+INSERT INTO links(source,target) VALUES ('10','11');
+INSERT INTO links(source,target) VALUES ('10','15');
+INSERT INTO links(source,target) VALUES ('11','OK20');
+INSERT INTO links(source,target) VALUES ('11','12');
+INSERT INTO links(source,target) VALUES ('12','Täffä');
+INSERT INTO links(source,target) VALUES ('12','13');
+INSERT INTO links(source,target) VALUES ('13','Dipoli');
+INSERT INTO links(source,target) VALUES ('13','14');
+INSERT INTO links(source,target) VALUES ('14','A Blanc');
+INSERT INTO links(source,target) VALUES ('15','JMT1');
+INSERT INTO links(source,target) VALUES ('15','16');
+INSERT INTO links(source,target) VALUES ('16','OK18');
+INSERT INTO links(source,target) VALUES ('16','17');
+INSERT INTO links(source,target) VALUES ('17','Teekkarimuseo');
+INSERT INTO links(source,target) VALUES ('17','18');
+INSERT INTO links(source,target) VALUES ('18','JMT6');
+INSERT INTO links(source,target) VALUES ('18','19');
+INSERT INTO links(source,target) VALUES ('19','JMT11');
---
+++ db/nodes.sql
@@ -0,0 +1,42 @@
+CREATE TABLE nodes(
+   id        INTEGER PRIMARY KEY AUTO_INCREMENT,
+   type      VARCHAR(200) NOT NULL
+  ,name      VARCHAR(200) NOT NULL
+  ,latitude  VARCHAR(200) NOT NULL
+  ,longitude VARCHAR(200) NOT NULL
+);
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('producer','St1','60,18803446','24,82800426');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('consumer','Kandikeskus','60,18653275','24,82832467');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('consumer','Nanotalo','60,18698854','24,82484653');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('consumer','Konetalo','60,18723004','24,82669374');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('consumer','Sähkötalo','60,18901735','24,83140802');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('consumer','Kvarkki','60,18824527','24,8301013');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('consumer','Raksatalo','60,18732835','24,8315244');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('consumer','OK20','60,18679975','24,83373056');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('consumer','Täffä','60,18607292','24,83297745');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('consumer','Dipoli','60,18512581','24,83247243');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('consumer','A Blanc','60,18468969','24,83032829');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('consumer','OK18','60,18809503','24,83551164');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('consumer','JMT1','60,18691892','24,83484714');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('consumer','Teekkarimuseo','60,18732858','24,83637993');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('consumer','JMT6','60,18849146','24,8373634');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('consumer','JMT11','60,18938122','24,83902024');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','1','60,18780909','24,8285705');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','2','60,1870675','24,82765256');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','3','60,18676515','24,82717064');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','4','60,1873185','24,82508232');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','5','60,18868758','24,82953434');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','6','60,18857349','24,83054407');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','7','60,18856208','24,8316915');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','8','60,18847081','24,83296514');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','9','60,18843088','24,83372244');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','10','60,18770641','24,83319462');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','11','60,18698763','24,83287334');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','12','60,18617185','24,83229963');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','13','60,18532183','24,8311063');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','14','60,1847228','24,83080797');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','15','60,18766078','24,83527146');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','16','60,18764937','24,83566158');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','17','60,18763796','24,83643036');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','18','60,18843089','24,83794496');
+INSERT INTO nodes(type,name,latitude,longitude) VALUES ('junction','19','60,18948619','24,83871373');
