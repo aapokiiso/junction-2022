cc3b0d5f7efb34d62cce71e0793d48d277165bd1
ViljamiN <viljami.narinen@aalto.fi>
2022-11-06 00:51:37 +0200

    favicon added

--- packages/frontend/pages/_app.js
+++ packages/frontend/pages/_app.js
@@ -43,7 +43,7 @@ function App() {
         <Head>
           <title>District Heating</title>
           <meta charSet="utf-8" />
-          <link rel="icon" href="favicon.ico" />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="theme-color" content="#000000" />
           <meta
