8dd7915cc2a0e706d9f383de28ac7b3a0a69aabb
Aapo Kiiso <aapo@kii.so>
2022-11-05 11:39:58 +0200

    Init Mapbox (#4)

---
+++ packages/frontend/components/Map.js
@@ -0,0 +1,25 @@
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import styles from './Map.module.css'
+
+import {useRef, useEffect} from 'react'
+
+function Map() {
+    const mapContainer = useRef(null)
+    const map = useRef(null)
+
+    useEffect(() => {
+        mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN
+
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+        })
+    })
+
+    return (
+        <div className={styles.mapContainer} ref={mapContainer}></div>
+    )
+}
+
+export default Map
