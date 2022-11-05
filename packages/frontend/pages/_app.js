ba7fb11c27bbc77f2335e88fdb7383f45b2fb6c0
ViljamiN <viljami.narinen@aalto.fi>
2022-11-06 00:46:37 +0200

    map should work

--- packages/frontend/pages/_app.js
+++ packages/frontend/pages/_app.js
@@ -43,6 +43,7 @@ function App() {
         <Head>
           <title>District Heating</title>
           <meta charSet="utf-8" />
+          <link rel="icon" href="favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="theme-color" content="#000000" />
           <meta
