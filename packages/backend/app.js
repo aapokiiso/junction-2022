99b2dc8ddf3c4a41c65e2d283f5e5eb8e441d780
Aapo Kiiso <aapo@lamia.fi>
2022-11-05 21:02:55 +0200

    Add cors to backend

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -2,9 +2,11 @@ const express = require('express')
 require("dotenv").config();
 const mysql = require('mysql2')
 const Sequelize = require("sequelize");
+const cors = require('cors')
 
 const app = express()
 app.use(express.json());
+app.use(cors())
 
 const port = process.env.PORT || 3000
 
