b254075a25b986b6d696e21d6edf2df52873b860
ViljamiN <viljami.narinen@aalto.fi>
2022-11-05 22:31:59 +0200

    react app addition continues

--- packages/frontend/pages/index.js
+++ packages/frontend/pages/index.js
@@ -1,7 +1,18 @@
 import React, {useEffect, useState} from 'react'
 import Map from '../components/Map'
 import {fetchNodes, fetchEdges} from '../utils/api'
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 import styles from './index.module.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
 
 function App () {
   const [nodes, setNodes] = useState(null);
