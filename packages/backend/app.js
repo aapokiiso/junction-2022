9ecd59ede275ce5d1f2885a1c941d2a2294db621
Aapo Kiiso <aapo@lamia.fi>
2022-11-05 16:08:40 +0200

    Support App Engine port in backend deploy

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2')
 const Sequelize = require("sequelize");
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
 
